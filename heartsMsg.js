e453aa6a9fd429fd7371f72a452b8ca572abe6c1
Chris <clj@ollure.com>
2022-02-08 23:44:34 -0600

    new msg

--- heartsMsg.js
+++ heartsMsg.js
@@ -3,11 +3,16 @@ var quotes = [
 	"\"Be the Fizz to my buzz!\" ",
 	"\“This ♥\" ",
 	"\“look stickers.\" ",
-	"\“We go together like Foo and Bar\" ",
+	"\“We go together like \n Foo and Bar\" ",
 	"\"I will always read the docs.\" ",
-	"\"Foo underful.\" "
+	"\"Foo underful.\" ",
+	"\"You had me at hello world\" ",
+	"\"I git commit myself to you\" ",
+	"\"if ( (you + date) < 2) { call (me) }\" "
 
 ]
+/* add short code tips? 
+add own msg */
 
 
 function sweetHeartMsg() {
