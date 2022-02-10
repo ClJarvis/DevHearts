3a470189b90b2c4e7b901dccf6b129e67b484b56
Chris <clj@ollure.com>
2022-02-09 23:00:02 -0600

    added more msgs

--- heartsMsg.js
+++ heartsMsg.js
@@ -1,14 +1,18 @@
 var quotes = [
-	"\"Will you be my variable?\" ",
-	"\"Be the Fizz to my buzz!\" ",
-	"\“This ♥\" ",
-	"\“look stickers.\" ",
-	"\“We go together like \n Foo and Bar\" ",
-	"\"I will always read the docs.\" ",
-	"\"Foo underful.\" ",
-	"\"You had me at hello world\" ",
-	"\"I git commit myself to you\" ",
-	"\"if ( (you + date) < 2) { call (me) }\" "
+	"Will you be my variable? ",
+	"Be the Fizz to my buzz! ",
+	"This ♥",
+	"stickers! ",
+	"We go together like <br> Foo and Bar ",
+	"I will always read the docs.",
+	"Foounderful.",
+	"You had me at hello world",
+	"I git commit myself to you",
+	"if ( (you + date) < 2) {  </br>   call (me) <br> } ",
+	"You\”: \“My heart\" ",
+	"I will always write tests",
+	"git commit -m \":heart:\" ",
+	"This.heart = \“yours\” "
 
 ]
 /* add short code tips? 
